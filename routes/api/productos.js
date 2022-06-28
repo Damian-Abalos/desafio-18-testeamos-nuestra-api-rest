88ccc16b53fedaf8edb0e57707d20a97de71ae4c
Damian <damianabalos@hotmail.com>
2022-06-28 20:45:11 -0300

    endpoints api/productos

--- routes/api/productos.js
+++ routes/api/productos.js
@@ -1,38 +1,26 @@
 import express from 'express'
-import ProductosRepo from "../../repositories/ProductosRepo.js";
-import Producto from "../../modelos/Producto.js";
-import ProductoMostrable from '../../adaptadores/ProductoMostrable.js';
 import getDao from '../../daos/ProductosDaoFactory'
 const apiProducts = express.Router()
-
-// const repo = new ProductosRepo();
-// const p1 = new Producto({
-//     id: 1,
-//     nombre: 'sonrisas',
-//     precio: 100,
-//     stock: 100
-//   })
-  
-//   const p2 = new Producto({
-//     id: 2,
-//     nombre: 'tita',
-//     precio: 80,
-//     stock: 100
-//   })
-//   await repo.add(p1)
-//   await repo.add(p2)
-  
-  function mostrarTodos(prods) {
-    const productos = []
-    prods.forEach(p => productos.push(new ProductoMostrable(p).comoTextoPlano()))
-    return productos
-}
-function mostrar(prod) {
-    console.log(new ProductoMostrable(prod).comoTextoPlano())
-}
 
 apiProducts.get('/api/productos', async (req, res) => {
     res.send(await getDao.getAll())
 })
+
+apiProducts.get('/api/productos/:id', async (req, res) => {
+    let id = req.params.id
+    res.send(await getDao.getById(id))
+})
+
+apiProducts.post('/api/productos', async (req, res) => {
+    let prod = req.body
+    res.send(await getDao.save(prod))
+})
+
+apiProducts.deleteById('/api/productos/:id', async (req, res) => {
+    let id = req.params.id
+    res.send(await getDao.save(id))
+})
+
+
 
 export default apiProducts
